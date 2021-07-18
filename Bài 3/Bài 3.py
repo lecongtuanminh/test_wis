ffe65efd75f2bdd2e59ba435855c7bbd12eebfc0
lecongtuanminh <82893280+lecongtuanminh@users.noreply.github.com>
2021-07-18 21:37:35 +0700

    Add files via upload

---
+++ Bài 3/Bài 3.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import math
+num = int(input("Nhập số: "))
+
+
+if num > 1:
+   
+   for i in range(2,(math.floor(math.sqrt(9))+1)):
+       if (num % i) == 0:
+           print(num,"không phải là số nguyên tố")
+           break
+   else:
+       print(num,"là số nguyên tốr")
+else:
+   print(num,"không phải là số nguyên tố")
+
+
+# In[ ]:
+
+
+
+
